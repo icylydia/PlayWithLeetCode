0191f41019bb37631fb84add7ae2a2186a7b11e8
LiChaoyi <icylydia@gmail.com>
2016-05-13 17:29:24 +0900

    24 unsolved

---
+++ Unsolved/24. Swap Nodes in Pairs/solution.swift
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+class Solution {
+    func swapPairs(head: ListNode?) -> ListNode? {
+    	var before = nil
+        var cur = head
+        var ans = head.next ?? head
+        while let a = cur, b = a.next {
+        	before?.next = b
+        	cur = b.next
+        	b.next = a
+        	a.next = nil
+        	before = a
+        }
+        return ans
+    }
+}
