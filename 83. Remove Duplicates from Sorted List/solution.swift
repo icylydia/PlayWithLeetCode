4a14377c4c5cadfdfb81175bc70568947ee7de13
LiChaoyi <icylydia@gmail.com>
2016-05-13 10:46:20 +0900

    83 code

--- 83. Remove Duplicates from Sorted List/solution.swift
+++ 83. Remove Duplicates from Sorted List/solution.swift
@@ -1,1 +1,25 @@
-solution.swift
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+class Solution {
+    func deleteDuplicates(head: ListNode?) -> ListNode? {
+    	if head?.next == nil {
+    		return head
+    	} else {
+    		if head!.val == head!.next!.val {
+    			return deleteDuplicates(head?.next)
+    		} else {
+    			head?.next = deleteDuplicates(head?.next)
+    			return head
+    		}
+    	}
+    }
+}
