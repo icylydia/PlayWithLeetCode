bfa0db7795523afe50278cb3464dcb20da3db2fa
LiChaoyi <icylydia@gmail.com>
2016-05-17 18:03:02 +0900

    176 code

---
+++ 176. Second Highest Salary/solution.sql
@@ -0,0 +1,9 @@
+# WRITE your MySQL query STATEMENT below
+SELECT IFNULL(
+                (SELECT Salary
+                 FROM Employee
+                 GROUP BY Salary
+                 ORDER BY Salary 
+                 DESC LIMIT 1 , 1)
+                , NULL) 
+AS `SecondHighestSalary`;
