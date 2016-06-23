efb62ac9a4722ebb761859e154fbb5440843a64a
LiChaoyi@Senko <icylydia@gmail.com>
2016-06-23 17:08:16 +0900

    313 code & Test Cases

---
+++ 313. Super Ugly Number/Solution.java
@@ -0,0 +1,79 @@
+public class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+    	if(n == 1) {
+    		return 1;
+    	}
+    	MinHeap heap = new MinHeap();
+    	heap.push(Long.valueOf(1));
+    	for(int i = 1; i < n ; i++){
+    		Long top = heap.pop();
+    		for(int j = primes.length - 1; j >= 0; j--) {
+    			int prime = primes[j];
+    			heap.push(top * prime);
+    			if(top % prime == 0) {
+    				break;
+    			}
+    		}
+    	}
+        return heap.top().intValue();
+    }
+    
+    class MinHeap {
+    	ArrayList<Long> elements = new ArrayList<Long>();
+    	public MinHeap() {
+    		elements.add(Long.valueOf(0));
+    	}
+    	public boolean isEmpty() {
+    		return elements.isEmpty();
+    	}
+    	public Long top() {
+    		return elements.get(1);
+    	}
+    	public Long pop() {
+    		if(elements.size() == 2){
+    			return elements.remove(1);
+    		}
+    		Long top = elements.get(1);
+    		Long tail = elements.remove(elements.size() -1);
+    		elements.set(1, tail);
+    		int index = 1;
+    		while((2*index+1) < elements.size()) {
+    			if(elements.get(2*index) < elements.get(2*index+1)) {
+	    			if(tail > elements.get(2*index)){
+	    				elements.set(index, elements.get(2*index));
+	    				elements.set(2*index, tail);
+	    				index = 2*index;
+    				} else {
+    					break;
+    				}
+    			} else {
+	    			if(tail > elements.get(2*index+1)){
+	    				elements.set(index, elements.get(2*index+1));
+	    				elements.set(2*index+1, tail);
+	    				index = 2*index + 1;
+    				} else {
+    					break;
+    				}
+    			}
+    		}
+    		if(2*index < elements.size() && tail > elements.get(2*index)) {
+				elements.set(index, elements.get(2*index));
+				elements.set(2*index, tail);
+    		}
+    		return top;
+    	}
+    	public void push(Long value) {
+    		elements.add(value);
+    		int index = elements.size() - 1;
+    		while(index > 1) {
+    			if(value < elements.get(index / 2)) {
+    				elements.set(index, elements.get(index / 2));
+    				elements.set(index / 2, value);
+    				index /= 2;
+    			} else {
+    				break;
+    			}
+    		}
+    	}
+    }
+}
