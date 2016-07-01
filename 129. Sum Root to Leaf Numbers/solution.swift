696bd02c57cdafa6e1d4eedfac423936dd1f2106
LiChaoyi@Senko <icylydia@gmail.com>
2016-07-01 09:44:29 +0900

    129 code

---
+++ 129. Sum Root to Leaf Numbers/solution.swift
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution {
+    func sumNumbers(root: TreeNode?) -> Int {
+        return helper(root)
+    }
+    func helper(root: TreeNode?, base: Int) -> Int {
+        if let root = root {
+            let cv = base * 10 + root.val
+            if root.left == nil && root.right == nil {
+                return cv
+            } else {
+                return helper(root.left, base: cv) + helper(root.right, base: cv)
+            }
+        } else {
+            return 0
+        }
+    }
+}
